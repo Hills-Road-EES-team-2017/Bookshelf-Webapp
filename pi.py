8c627ce4d7054a703d745754b465f1d496982676
Finn Sutcliffe <fiin.sutcliffe@gmail.com>
2018-04-22 21:09:48 +0100

    Fixed alignment on Base, Add, Maps .hmtl
    Signed-off-by: Finn Sutcliffe <fiin.sutcliffe@gmail.com>

--- pi.py
+++ pi.py
@@ -14,25 +14,24 @@ GPIO.setup(22,GPIO.IN)
 GPIO.setwarnings(False)
 
 
-# def API_Get_Button():
-#     if not GPIO.input(22): # RED Button
-#         return "R"
-#     if not GPIO.input(18): # GREEN Button
-#         return "G"
-#     if not GPIO.input(16): # YELLOW Button pressed
-#         return "Y"
-#     return None
-
 def API_Get_Button():
-    return input()
+    if not GPIO.input(22): # RED Button
+        return "R"
+    if not GPIO.input(18): # GREEN Button
+        return "G"
+    if not GPIO.input(16): # YELLOW Button pressed
+        return "Y"
+    return None
+
+# def API_Get_Button():
+#     return input()
 
 
 while True:
     colour = API_Get_Button()
     if colour:
         client = requests.session()
-        #print(colour, "button pressed")
         url = 'http://www.eeslibrary.com:8000/off/'+colour+'/'
+        # url ='192.168.137.2:8000/off'+colour+'/'
         r2 = client.get(url)
-        #print(colour, "web requested")
         time.sleep(0.3)
